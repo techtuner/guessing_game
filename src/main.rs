03db92a63e4b1182e854922699fb1c8cf9bb40f1
techtuner <techtuner284@gmail.com>
2022-11-27 20:26:11 +0530

    Add ASCII art greeting and fixed some issues

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,43 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number");
+    println!(
+        r#"                              _   _                                  _               "#
+    );
+    println!(
+        r#"                             | | | |                                | |              "#
+    );
+    println!(
+        r#"   __ _ _   _  ___  ___ ___  | |_| |__   ___   _ __  _   _ _ __ ___ | |__   ___ _ __ "#
+    );
+    println!(
+        r#"  / _` | | | |/ _ \/ __/ __| | __| '_ \ / _ \ | '_ \| | | | '_ ` _ \| '_ \ / _ \ '__|"#
+    );
+    println!(
+        r#" | (_| | |_| |  __/\__ \__ \ | |_| | | |  __/ | | | | |_| | | | | | | |_) |  __/ |   "#
+    );
+    println!(
+        r#"  \__, |\__,_|\___||___/___/  \__|_| |_|\___| |_| |_|\__,_|_| |_| |_|_.__/ \___|_|   "#
+    );
+    println!(
+        r#"   __/ |                                                                             "#
+    );
+    println!(
+        r#"  |___/                                                                              "#
+    );
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    let mut trials = 0;
+    let mut trials = 1;
     loop {
         let mut guess = String::new();
+        println!("Enter a number to guess: ");
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to load the content");
-
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-        print!("Enter the number: ");
         match guess.cmp(&secret_number) {
             Ordering::Less => {
                 trials = trials + 1;
@@ -24,7 +47,7 @@ fn main() {
             }
             Ordering::Equal => {
                 println!("You win!!!");
-                println!("You took {trials} to guess the number!!!!");
+                println!("You took {trials} turns to guess the number!!!!");
                 break;
             }
             Ordering::Greater => {
